0f1a5814f85dddeded16c102f5c68e8e7a30d085
Christoph Schläpfer <c.schlaepfer@datact.ch>
2025-01-10 10:00:28 +0100

    Fixed database dump for postgresql

--- upgrade.sh
+++ upgrade.sh
@@ -126,7 +126,7 @@ function backup_database
         "postgresql")
             echo "Backing up PostgreSQL database"
             echo "$(date '+%Y-%m-%d %H:%M:%S')> Backing up PostgreSQL database" >> $logfile
-            $db_client -U $DB_USER -h $DB_HOST -p $DB_PORT $DB_NAME > $HOME/$DB_NAME_$(date '+%Y%m%d%H%M%S').sql
+            $db_client -U $DB_USER -h $DB_HOST -p $DB_PORT $DB_NAME -W $DB_PASSWORD > $HOME/$DB_NAME_$(date '+%Y%m%d%H%M%S').sql
             ;;
         *)
             echo "Unsupported database type: $DB_TYPE"
@@ -149,7 +149,7 @@ function upgrade_vaultwarden {
     install_rust
 
     # Upgrade nodejs
-    install_nodejs
+    install_npm_w_deps
 
     # backup database
     backup_database
